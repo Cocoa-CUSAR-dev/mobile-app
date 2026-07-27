3be0f5cb7c2049626dbb79ac888a2ebea1880d8b
kwank6704 <nattarat.kwan@gmail.com>
2026-07-27 15:43:52 +0700

    Refactor pageTransitionsTheme to remove const modifier in ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
           fontFamily: 'NotoSansThaiLooped',
-          pageTransitionsTheme: const PageTransitionsTheme(
+          pageTransitionsTheme: PageTransitionsTheme(
             builders: {
               TargetPlatform.android: ZoomPageTransitionsBuilder(),
               TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
